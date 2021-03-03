a7574d56e89a4de3ba7a45c74a64efc956506c0a
alexsilva9913 <alexsilva9913@gmail.com>
2021-03-03 15:41:09 +0000

    Second Commit - Main Activity Done [LoginPage]

--- app/src/main/java/pt/ie/commov_helpapp/MainActivity.kt
+++ app/src/main/java/pt/ie/commov_helpapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package pt.ie.commov_helpapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ButNotas = findViewById<Button>(R.id.butNotas) as Button
+
+        //Abrir Notas
+        ButNotas.setOnClickListener {
+            val intent = Intent(this, ListaNotas::class.java)
+            startActivity(intent)
+        }
+
     }
 }
